be38c1b067c9541013586db36d07e06d1251a91f
AkintanSeyi <seyiakintan@yahoo.com>
2025-04-25 15:13:29 -0700

    port2025

--- src/data2.js
+++ src/data2.js
@@ -26,8 +26,8 @@ const people3 = [
     image:
     "5.png",
   name: 'slick-funiture',
-  git: 'https://github.com/Akingz2000/funiture',
-  web: 'https://akingz2000.github.io/funiture/',
+  git: 'https://github.com/akintanseyi/funiture',
+  web: 'https://akintanseyi.github.io/funiture/',
   p:  'this is a website of slick-funiture company, they sell different kind of funitures of all kind, they also offer delivey of product free and offer a great product at afforable rate'
 
   },
@@ -36,8 +36,8 @@ const people3 = [
     image:
     "11.png",
   name: 'fibre',
-  git: 'https://github.com/Akingz2000/fibre-',
-  web: 'https://akingz2000.github.io/fibre-/',
+  git: 'https://github.com/akintanseyi/fibre-',
+  web: 'https://akintanseyi.github.io/fibre-/',
   p:  'fibre is the number one leading housing company in lagos, having over 100 buildings and hundreds of agent nationwide'
 
 
@@ -47,8 +47,8 @@ const people3 = [
         image:
           "7.png",
         name: 'as-cars',
-        git: 'https://github.com/Akingz2000/car',
-        web: 'https://akingz2000.github.io/car/',
+        git: 'https://github.com/akintanseyi/car',
+        web: 'https://akintanseyi.github.io/car/',
         p: 'this is a website about as-cars company, top leading car dealers company in west-africa, they sold over 5000 cars and are know as revolutionarizing selling of cars'
      
       },
@@ -60,8 +60,8 @@ const people3 = [
         image:
         "4.png",
       name: 'mother-earth',
-      git: 'https://github.com/Akingz2000/mother-earth',
-      web: 'https://akingz2000.github.io/mother-earth/',
+      git: 'https://github.com/akintanseyi/mother-earth',
+      web: 'https://akintanseyi.github.io/mother-earth/',
       p: 'this is a webite about our beautiful planet, different amazing pictures from all over the world'
    
       },
@@ -70,8 +70,8 @@ const people3 = [
         image:
         "6.png",
       name: 'housing in lagos',
-      git: 'https://github.com/Akingz2000/housing-website',
-      web: 'https://akingz2000.github.io/housing-website/',
+      git: 'https://github.com/akintanseyi/housing-website',
+      web: 'https://akintanseyi.github.io/housing-website/',
       p:  'housing in lagos is an new compny with just took off 2 years ago, they are making waves and are know for providing houses in lagos state'
    
     
@@ -81,8 +81,8 @@ const people3 = [
         image:
         "8.png",
       name: 'nft-store',
-      git: 'https://github.com/Akingz2000/nft',
-      web: 'https://akingz2000.github.io/nft/',
+      git: 'https://github.com/akintanseyi/nft',
+      web: 'https://akintanseyi.github.io/nft/',
       p:  'this is a website about nft pictures and beautiful pictures from different nft products'
    
    
@@ -94,8 +94,8 @@ const people3 = [
           image:
           "9.png",
         name: 'freash-menu',
-        git: 'https://github.com/Akingz2000/freshmenu',
-        web: 'https://akingz2000.github.io/freshmenu/',
+        git: 'https://github.com/akintanseyi/freshmenu',
+        web: 'https://akintanseyi.github.io/freshmenu/',
         p:  'this is a new india resturant which is making waves right now, have over 20 stores nationwide and make amazing meals'
      
      
